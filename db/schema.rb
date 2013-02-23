139ab7977ed60d022027be0e480c9a475cfe5e24
Oleg Pasko <olegpasko@gmail.com>
2013-02-23 13:20:07 +0200

    cover added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130223111601) do
+ActiveRecord::Schema.define(:version => 20130223111934) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130223111601) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "cover"
   end
 
   create_table "pages", :force => true do |t|
